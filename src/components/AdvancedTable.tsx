7111bb2006e625bce93a0e40273fbfc8fa13f3fe
Husniddin Sopoyev <129381681+Husniddin0111@users.noreply.github.com>
2025-06-23 18:58:09 +0500

    test

--- src/components/AdvancedTable.tsx
+++ src/components/AdvancedTable.tsx
@@ -96,7 +96,7 @@ const AdvancedTable = () => {
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
               <Input
                 type="text"
-                placeholder="Searche"
+                placeholder="Search"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="pl-10 w-80 bg-gray-50 border-gray-200"
